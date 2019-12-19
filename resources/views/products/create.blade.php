effbc96ecb14be8b9c8ab503a91d380126e72416
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-19 12:21:11 +0900

    add products create action

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,6 @@
+<form method="post" action="/products">
+    <input type="text" name="name">
+    <textarea name="description"></textarea>
+    <input type="number" name="price">
+    <input type="submit" value="Add">
+</form>
