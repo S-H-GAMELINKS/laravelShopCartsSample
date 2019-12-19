971eff759962f9c0cc74f198c286d21463405976
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-19 14:31:27 +0900

    publish carts

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,3 +3,12 @@
 {{ $product->description }}
 
 {{ $product->price }}
+
+<form method="post" action="/carts">
+    @csrf
+    <input type="hidden" name="id" value="{{ $product->id }}">
+    <input type="hidden" name="name" value="{{ $product->name }}">
+    <input type="hidden" name="qty" value="1">
+    <input type="hidden" name="price" value="{{ $product->price }}">
+    <input type="submit">
+</form>
