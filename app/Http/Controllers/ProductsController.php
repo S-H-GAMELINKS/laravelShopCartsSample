7f79fa91c7170bd549819e1444ceba8c06c353cc
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-19 12:32:29 +0900

    add products show action

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -52,7 +52,7 @@ class ProductsController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     /**
